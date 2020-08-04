a9f85b616538f75a1f64acab7d08cd83750a9b64
Lucas Rhoden <62027315+lucasrhoden@users.noreply.github.com>
2020-08-04 13:30:40 +0100

    Updates

---
+++ precache-manifest.7394059a42a85470993c06e2a2b99768.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a466a56467d0d5d63eae0209c9fadf64",
+    "url": "/Hooks-App/index.html"
+  },
+  {
+    "revision": "4a135d720768d5075066",
+    "url": "/Hooks-App/static/css/main.b34c7663.chunk.css"
+  },
+  {
+    "revision": "6806dadc9555a233b374",
+    "url": "/Hooks-App/static/js/2.5844e9f7.chunk.js"
+  },
+  {
+    "revision": "4a135d720768d5075066",
+    "url": "/Hooks-App/static/js/main.45fb9ad2.chunk.js"
+  },
+  {
+    "revision": "718fa70635bf374a6316",
+    "url": "/Hooks-App/static/js/runtime~main.60c5bf79.js"
+  }
+]);
