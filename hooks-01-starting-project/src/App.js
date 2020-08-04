9c215bfc4a9370d0aeab05c901c87111af326c20
Lucas Rhoden <62027315+lucasrhoden@users.noreply.github.com>
2020-08-04 13:07:51 +0100

    Add Router

--- hooks-01-starting-project/src/App.js
+++ hooks-01-starting-project/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import { Route } from 'react-router-dom'
 import Ingredients from './components/Ingredients/Ingredients';
 
 const App = props => {
-  return <Ingredients />;
+  return <Route path="/" exact component={Ingredients} />;
 };
 
 export default App;
